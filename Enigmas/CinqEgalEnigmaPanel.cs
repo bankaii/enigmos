cf9605342cd1e31192569a0e61f1c8ef0780d82b
Ben Salah Jamil <BenSalahJa@s2.rpn.ch>
2015-12-18 09:08:04 +0100

    fini l'énigme, mais perfectible (pourquoi pas faire tableau)

--- Enigmas/CinqEgalEnigmaPanel.cs
+++ Enigmas/CinqEgalEnigmaPanel.cs
@@ -9,6 +9,13 @@ namespace Cpln.Enigmos.Enigmas
     /// </summary>
     public class CinqEgalEnigmaPanel : EnigmaPanel
     {
+        private System.Windows.Forms.TableLayoutPanel centerLayout;
+        private System.Windows.Forms.Label lblDonnee1;
+        private System.Windows.Forms.Label lblDonnee2;
+        private System.Windows.Forms.Label lblDonnee3;
+        private System.Windows.Forms.Label lblDonnee4;
+        private System.Windows.Forms.Label lblDonnee5;
+
         /// <summary>
         /// Constructeur par défaut, génère un texte et l'affiche dans le Panel.
         /// </summary>
@@ -20,7 +27,7 @@ namespace Cpln.Enigmos.Enigmas
             Label lblDonnee3 = new Label();
             Label lblDonnee4 = new Label();
             Label lblDonnee5 = new Label();
-
+      
 
             lblDonnee1.Text = "1 = 5";
             lblDonnee2.Text = "2 = 25";
@@ -28,62 +35,42 @@ namespace Cpln.Enigmos.Enigmas
             lblDonnee4.Text = "4 = 4325";
             lblDonnee5.Text = "5 = ?";
 
+            lblDonnee1.ForeColor = Color.Blue;
+            lblDonnee2.ForeColor = Color.Red;
+            lblDonnee3.ForeColor = Color.Purple;
+            lblDonnee4.ForeColor = Color.Green;
+            lblDonnee5.ForeColor = Color.Black;
+
+            lblDonnee1.Font = new Font("Arial", 15);
+            lblDonnee2.Font = new Font("Arial", 15);
+            lblDonnee3.Font = new Font("Arial", 15);
+            lblDonnee4.Font = new Font("Arial", 15);
+            lblDonnee5.Font = new Font("Arial", 15);
+
+
             centerLayout.ColumnCount = 3;
             centerLayout.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 0.5f));
             centerLayout.ColumnStyles.Add(new ColumnStyle(SizeType.AutoSize));
+
             centerLayout.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 0.5f));
-            centerLayout.RowCount = 3;
-            centerLayout.RowStyles.Add(new RowStyle(SizeType.Percent, 0.5f));
-            centerLayout.RowStyles.Add(new RowStyle(SizeType.AutoSize));
-            centerLayout.RowStyles.Add(new RowStyle(SizeType.Percent, 0.5f));
+            centerLayout.RowCount = 7;
+            centerLayout.RowStyles.Add(new RowStyle(SizeType.Percent, 37));
+            centerLayout.RowStyles.Add(new RowStyle(SizeType.Percent, 5));
+            centerLayout.RowStyles.Add(new RowStyle(SizeType.Percent, 5));
+            centerLayout.RowStyles.Add(new RowStyle(SizeType.Percent, 5));
+            centerLayout.RowStyles.Add(new RowStyle(SizeType.Percent, 5));
+            centerLayout.RowStyles.Add(new RowStyle(SizeType.Percent, 5));
+            centerLayout.RowStyles.Add(new RowStyle(SizeType.Percent, 38));
             centerLayout.Dock = DockStyle.Fill;
-            centerLayout.Controls.Add(lblDonnee1);
-            centerLayout.Controls.Add(lblDonnee2);
-            centerLayout.Controls.Add(lblDonnee3);
-            centerLayout.Controls.Add(lblDonnee4);
-            centerLayout.Controls.Add(lblDonnee5);
+            Controls.Add(centerLayout);
 
-            Controls.Add(lblDonnee1);
-            Controls.Add(lblDonnee2);
-            Controls.Add(lblDonnee3);
-            Controls.Add(lblDonnee4);
-            Controls.Add(lblDonnee5);
+            centerLayout.Controls.Add(lblDonnee1, 1, 1);
+            centerLayout.Controls.Add(lblDonnee2, 1, 2);
+            centerLayout.Controls.Add(lblDonnee3, 1, 3);
+            centerLayout.Controls.Add(lblDonnee4, 1, 4);
+            centerLayout.Controls.Add(lblDonnee5, 1, 5);
 
-            
-           /* lblDonnee1.Font = new Font(FontFamily.GenericSansSerif, 24, FontStyle.Bold);
-            lblDonnee1.ForeColor = Color.Green;
-            lblDonnee1.Location = new Point(200, 100);
-            lblDonnee1.AutoSize = false;
-            lblDonnee1.Size = TextRenderer.MeasureText(lblDonnee1.Text, lblDonnee1.Font);
-
-
-
-            lblDonnee2.Font = new Font(FontFamily.GenericSansSerif, 24, FontStyle.Bold);
-            lblDonnee2.ForeColor = Color.Blue;
-            lblDonnee2.Location = new Point(200, 150);
-            lblDonnee2.AutoSize = false;
-            lblDonnee2.Size = TextRenderer.MeasureText(lblDonnee2.Text, lblDonnee2.Font);
-
-
-
-            lblDonnee3.Font = new Font(FontFamily.GenericSansSerif, 24, FontStyle.Bold);
-            lblDonnee3.ForeColor = Color.Red;
-            lblDonnee3.Location = new Point(200, 200);
-            lblDonnee3.AutoSize = false;
-            lblDonnee3.Size = TextRenderer.MeasureText(lblDonnee3.Text, lblDonnee3.Font);
-
-
-            lblDonnee4.Font = new Font(FontFamily.GenericSansSerif, 24, FontStyle.Bold);
-            lblDonnee4.ForeColor = Color.Black;
-            lblDonnee4.Location = new Point(200, 250);
-            lblDonnee4.AutoSize = false;
-            lblDonnee4.Size = TextRenderer.MeasureText(lblDonnee4.Text, lblDonnee4.Font);
-
-            lblDonnee5.Font = new Font(FontFamily.GenericSansSerif, 24, FontStyle.Bold);
-            lblDonnee5.ForeColor = Color.Purple;
-            lblDonnee5.Location = new Point(200, 300);
-            lblDonnee5.AutoSize = false;
-            lblDonnee5.Size = TextRenderer.MeasureText(lblDonnee5.Text, lblDonnee5.Font);*/
+           
             
            
         }
